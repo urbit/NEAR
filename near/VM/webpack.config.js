10b17984682a3b9c5a77398b4b1279ccc6e2a905
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-02-13 18:02:44 -0800

    working monorepo

--- near/VM/webpack.config.js
+++ near/VM/webpack.config.js
@@ -63,7 +63,7 @@ module.exports = function (env) {
           stream: require.resolve("stream-browserify"),
           util: require.resolve("util"),
           punycode: require.resolve("punycode"),
-          events: require.resolve("events")
+          // events: require.resolve("events")
         },
       },
       target: "node",
