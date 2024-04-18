384ffc7605f449ef8104bc72e4dd50443823035f
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2024-04-18 16:59:49 +0100

    Use GatewayCards for published gateways

--- ui/src/components/GatewayCard.jsx
+++ ui/src/components/GatewayCard.jsx
@@ -1,57 +1,64 @@
 import React from 'react'
+import useUiState from '../../state/useUiState'
+import useGatewaysState from '../../state/useGatewayState'
 
 function getImage(url) {
   // TODO get image and fallback if bad response
   return url
 }
 
-// TODO use id prop
-function GatewayCard({ imageUrl, title, author, description, link, isPublished, isInstalled }) {
+function GatewayCard({ key, gateway, isPublished, isInstalled }) {
+  const { setInstGateway } = useGatewaysState()
+  const { setShowDelete, setInstallWindow } = useUiState()
+
   function handleInstallClick() {
-    // return (
-    //   <InstallGatewayModal
-    //     gateway={{
-    //       id: id,
-    //       name: title,
-    //       ship: author,
-    //       url: link,
-    //       about: description
-    //     }}
-    //   />
-    // )
+    setInstGateway(gateway)
+    setInstallWindow(true)
   }
 
   function handleOpenClick() {
-    window.open(`${link}`, '_blank', 'noopener,noreferrer')
+    window.open(
+      `${window.location.origin}/apps/near/${gateway.ship}/${gateway.id}/gateway/`,
+      '_blank',
+      'noopener,noreferrer'
+    )
   }
 
   function handleDeleteClick() {
-
+    setDelGateway(gateway)
+    setShowDelete(true)
   }
 
   function handleUnpublishClick() {
-
+    setDelGateway(gateway)
+    // TODO accomodate unpublishing in UI / copy
+    setShowDelete(true)
   }
 
   return (
-    <div>
-      <img src={getImage(imageUrl)} alt={`${title} preview image`} />
+    <div key={key} className='gateway-container'>
+      <img
+        src={getImage('https://pbs.twimg.com/profile_images/1631021064171196431/_ahCp9jR_400x400.jpg')}
+        alt={`${gateway.name} preview image`}
+      />
       <div className='info'>
-        <h2 className='title'>{title}</h2>
-        <h3 className='ship'>{author}</h3>
-        <h4 className='text'>{description}</h4>
+        <h2 className='title'>{gateway.name}</h2>
+        <h3 className='ship'>{gateway.ship}</h3>
+        <h4 className='text'>{gateway.about}</h4>
       </div>
-      {isInstalled &&
-      <button onClick={() => {
-        isPublished ? handleUnpublishClick : handleDeleteClick
-        }}>
-        {isPublished ? 'Unpublish' : 'Delete'}
-      </button>}
-      <button onClick={() => {
-        isInstalled ? handleOpenClick : handleInstallClick
-        }}>
-        {isInstalled ? 'Open' : 'Install'}
-      </button>
+      <div className="git">
+        {isInstalled &&
+        <button onClick={() => {
+          isPublished ? handleUnpublishClick : handleDeleteClick
+          }}>
+          {isPublished ? 'Unpublish' : 'Delete'}
+        </button>}
+        <button onClick={() => {
+          isInstalled ? handleOpenClick : handleInstallClick
+          }}>
+          {isInstalled ? 'Open' : 'Install'}
+        </button>
+      </div>
     </div>
   )
 }
--- ui/src/components/PublishedGateways.jsx
+++ ui/src/components/PublishedGateways.jsx
@@ -3,9 +3,10 @@ import NewGateway from './NewGateway.jsx'
 import useUiState from '../../state/useUiState.js'
 import useGatewaysState from '../../state/useGatewayState.js'
 import { scryPublished } from '../api/scries.js'
+import GatewayCard from './GatewayCard.jsx'
 
 function PublishedGateways() {
-  const { published, setPublished, setDelGateway } = useGatewaysState()
+  const { installed, published, setPublished, setDelGateway } = useGatewaysState()
   const { loading, showNew, setShowNew, setShowDelete } = useUiState()
 
   useEffect(() => {
@@ -51,29 +52,40 @@ function PublishedGateways() {
       <div>
         <div className='flex-box'>
           {published.map((gateway, index) => {
-            let name = gateway.name
-            let url = `${window.location.origin}/apps/near/${gateway.ship}/${gateway.id}/gateway/`
+            // let url = `${window.location.origin}/apps/near/${gateway.ship}/${gateway.id}/gateway/`
 
             return (
-              <div key={index} className='gateway-container'>
-                <iframe src={url} title={url} className='frame'></iframe>
-                <div className='info'>
-                  <h2 className='name' href={url}>{name}</h2>
-                  <h3 className='ship'>{gateway.ship}</h3>
-                  <h4 className='text'>{gateway.about}</h4>
-                </div>
-                <div className="git">
-                  <button onClick={() => {
-                    setShowNew(false)
-                    setShowDelete(true)
-                    setDelGateway(gateway)
-                  }}>
-                    Delete
-                  </button>
-                  <a href={url}>Gateway</a>
-                </div>
-              </div>
-            )})
+              <GatewayCard
+                key={index}
+                gateway={gateway}
+                isPublished={true}
+                isInstalled={installed.some(installedGateway => {
+                  gateway.id === installedGateway.id
+                })}
+              />
+            )
+
+            // return (
+            //   <div key={index} className='gateway-container'>
+            //     <iframe src={url} title={url} className='frame'></iframe>
+            //     <div className='info'>
+            //       <h2 className='name' href={url}>{name}</h2>
+            //       <h3 className='ship'>{gateway.ship}</h3>
+            //       <h4 className='text'>{gateway.about}</h4>
+            //     </div>
+            //     <div className="git">
+            //       <button onClick={() => {
+            //         setShowNew(false)
+            //         setShowDelete(true)
+            //         setDelGateway(gateway)
+            //       }}>
+            //         Delete
+            //       </button>
+            //       <a href={url}>Gateway</a>
+            //     </div>
+            //   </div>
+            // )
+          })
           }
           <div className='add-gateway' onClick={() => {
             setShowNew(true)
