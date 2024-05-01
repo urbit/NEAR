316d63e549d32da20dd3687c684d3d2dc1d954a8
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2024-05-01 10:51:42 +0100

    Amend install copy

--- ui/src/components/InstallGatewayModal.jsx
+++ ui/src/components/InstallGatewayModal.jsx
@@ -12,9 +12,6 @@ function InstallGatewayModal() {
       <h2 className='paragraph'>
         BOS gateways can access all data on your ship, just like any other Urbit app. Only install gateways from developers you trust!
       </h2>
-      <h2 className='paragraph-2'>
-        Would you like to install {instGateway.name}?
-      </h2>
       <div className="inst-buttons">
         <button onClick={async () => {installGateway(instGateway)}}>
           Install
